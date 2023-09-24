e1305d9c373fd608f072cea48bfe368001a3abac
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-24 18:53:59 -0300

    criação da classe estado

---
+++ src/main/java/com/kaique/projetoLoja/domain/Estado.java
@@ -0,0 +1,35 @@
+package com.kaique.projetoLoja.domain;
+
+import java.io.Serializable;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "tb_estado")
+public class Estado implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nome;
+
+}
