2df402a3b2bc9bd206560e1e58380fe533c048f7
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-24 20:33:26 -0300

    populando banco de dados para teste

--- src/main/java/com/kaique/projetoLoja/config/Testconfig.java
+++ src/main/java/com/kaique/projetoLoja/config/Testconfig.java
@@ -7,8 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.kaique.projetoLoja.domain.Categoria;
+import com.kaique.projetoLoja.domain.Cidade;
+import com.kaique.projetoLoja.domain.Estado;
 import com.kaique.projetoLoja.domain.Produto;
 import com.kaique.projetoLoja.repositories.CategoriaRepository;
+import com.kaique.projetoLoja.repositories.CidadeRepository;
+import com.kaique.projetoLoja.repositories.EstadoRepository;
 import com.kaique.projetoLoja.repositories.ProdutoRepository;
 
 @Configuration
@@ -20,7 +24,11 @@ public class Testconfig implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -31,7 +39,7 @@ public class Testconfig implements CommandLineRunner {
 		Produto prod2 = new Produto(null, "impressora", 800.00);
 		Produto prod3 = new Produto(null, "mouse", 80.00);
 
-		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3 )); 
+		
 		
 		p1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
 		p2.getProdutos().addAll(Arrays.asList(prod2));
@@ -39,9 +47,22 @@ public class Testconfig implements CommandLineRunner {
 		prod1.getCategorias().addAll(Arrays.asList(p1));
 		prod2.getCategorias().addAll(Arrays.asList(p1, p2));
 		prod3.getCategorias().addAll(Arrays.asList(p1));
-
+		
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3 )); 
 		repository.saveAll(Arrays.asList(p1, p2));
 		
+		Estado est1 = new Estado(null , "Minas gerais");
+		Estado est2 = new Estado(null , "São Paulo");
+		
+		Cidade c1 = new Cidade(null , "Uberlandia" , est1);
+		Cidade c2 = new Cidade(null , "São Paulo" , est2);
+		Cidade c3 = new Cidade(null , "Camplinas" , est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2 , c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1 , est2));
+		cidadeRepository.saveAll(Arrays.asList(c1 , c2 , c3));
 	}
 
 }
