337919a3a1a28251c60b69ac29b0a9419ee92178
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-24 20:03:55 -0300

    criação de estadoService e estadoResources , crud completo

---
+++ src/main/java/com/kaique/projetoLoja/resources/EstadoResources.java
@@ -0,0 +1,60 @@
+package com.kaique.projetoLoja.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kaique.projetoLoja.domain.Estado;
+import com.kaique.projetoLoja.services.EstadoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoResources {
+
+	@Autowired
+	private EstadoService service;
+
+	@GetMapping
+	public ResponseEntity<List<Estado>> findAll() {
+		List<Estado> listaEstados = service.findAll();
+		return listaEstados.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+				: ResponseEntity.ok().body(listaEstados);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Estado> findById(@PathVariable Long id) {
+		Estado estado = service.findById(id);
+
+		return estado == null ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+				: ResponseEntity.ok().body(estado);
+	}
+
+	@PostMapping
+	public ResponseEntity<Estado> sava(@Valid @RequestBody Estado estado) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(estado));
+	}
+
+	@PutMapping
+	public ResponseEntity<Estado> update(@Valid @RequestBody Estado estado) {
+		return service.update(estado) != null ? ResponseEntity.status(HttpStatus.CREATED).body(estado)
+				: ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		return service.delete(id) == true ? ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+				: ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+}
---
+++ src/main/java/com/kaique/projetoLoja/services/EstadoService.java
@@ -0,0 +1,45 @@
+package com.kaique.projetoLoja.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaique.projetoLoja.domain.Cidade;
+import com.kaique.projetoLoja.domain.Estado;
+import com.kaique.projetoLoja.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+
+	@Autowired
+	private EstadoRepository repository;
+
+	public List<Estado> findAll() {
+		return repository.findAll();
+	}
+
+	public Estado findById(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	public Estado save(Estado estado) {
+		return repository.save(estado);
+	}
+
+	public Estado update(Estado estado) {
+		return repository.findById(estado.getId()).map(x -> {
+			x.setNome(estado.getNome());
+			return repository.save(x);
+		}).orElse(null);
+	}
+
+	public Boolean delete(Long id) {
+		if (repository.findById(id).isPresent()) {
+			repository.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
