0c62c03e3e82779ffd9524c94c243775ced34c49
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-24 19:43:57 -0300

    repository estado 1.0

--- src/main/java/com/kaique/projetoLoja/repositories/EstadoRepository.java
+++ src/main/java/com/kaique/projetoLoja/repositories/EstadoRepository.java
@@ -2,8 +2,8 @@ package com.kaique.projetoLoja.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.kaique.projetoLoja.domain.Produto;
+import com.kaique.projetoLoja.domain.Estado;
 
-public interface EstadoRepository extends JpaRepository<Produto, Long>{
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
 
 }
