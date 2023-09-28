610dd110c2beddea1c5830703457f2998f64c9ad
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-28 18:48:47 -0300

    interface pagamentoComBoleto

---
+++ src/main/java/com/kaique/projetoLoja/repositories/PagamentoComBoletoRepository.java
@@ -0,0 +1,9 @@
+package com.kaique.projetoLoja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kaique.projetoLoja.domain.PagamentoComBoleto;
+
+public interface PagamentoComBoletoRepository extends JpaRepository<PagamentoComBoleto, Long>{
+
+}
