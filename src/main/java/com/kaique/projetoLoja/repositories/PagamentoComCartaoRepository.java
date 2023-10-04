208337ab83d72b77d3010e12ecdcd471df30aee9
kaiquesilvadev <j.kaique1570@gmail.com>
2023-10-04 19:27:57 -0300

    PagamentoComCartaoRepository

---
+++ src/main/java/com/kaique/projetoLoja/repositories/PagamentoComCartaoRepository.java
@@ -0,0 +1,9 @@
+package com.kaique.projetoLoja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kaique.projetoLoja.domain.PagamentoComCartao;
+
+public interface PagamentoComCartaoRepository extends JpaRepository<PagamentoComCartao, Long>{
+
+}
